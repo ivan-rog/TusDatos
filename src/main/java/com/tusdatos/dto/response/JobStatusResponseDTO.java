7fdc90b41c6e82642a7eeb0807c0d0bd877a9a8a
ivan-rog <ivan.romero.develop@gmail.com>
2025-02-23 15:34:46 -0600

    feat: API request and response - JobStatus

---
+++ src/main/java/com/tusdatos/dto/response/JobStatusResponseDTO.java
@@ -0,0 +1,34 @@
+package com.tusdatos.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tusdatos.dto.enums.DocumentTypes;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class JobStatusResponseDTO {
+
+    private String cedula;
+    private boolean error;
+    @JsonProperty("errores")
+    private List<String> errors;
+    @JsonProperty("estado")
+    private String status;
+    @JsonProperty("hallazgo")
+    private boolean finding;
+    @JsonProperty("hallazgos")
+    private String findings;
+    private String id;
+    @JsonProperty("nombre")
+    private String name;
+    private Object result;
+    private long time;
+    @JsonProperty("typedoc")
+    private DocumentTypes documentTypes;
+    @JsonProperty("validado")
+    private boolean validated;
+
+}
